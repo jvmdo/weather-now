dcbbd68a62d34842a2e8ab202fb860f9311e52e8
João Oliveira <deoliveira.jvm@gmail.com>
2025-11-03 18:08:09 -0300

    ✨ Geocoding search filtering and pagination

--- src/components/CitySearch/CitySearch.jsx
+++ src/components/CitySearch/CitySearch.jsx
@@ -1,24 +1,92 @@
 import React from "react";
-import { Command } from "cmdk";
+import { Command, useCommandState } from "cmdk";
+import Fuse from "fuse.js";
 
 function CitySearch() {
+  const [searchTerm, setSearchTerm] = React.useState("new york");
+  const [payload, setPayload] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [page, setPage] = React.useState(10);
+
+  const fuse = new Fuse(payload, {
+    includeScore: true,
+    useExtendedSearch: true,
+    keys: [
+      { name: "name", weight: 10 },
+      { name: "admin1" },
+      { name: "admin2" },
+      { name: "admin3" },
+      { name: "country" },
+    ],
+  });
+  const [name, admin, country] = searchTerm.split(",").map((t) => t.trim());
+  const items = fuse.search({
+    $or: [
+      { name: `${name}` },
+      { admin1: `${admin}` },
+      { admin2: `${admin}` },
+      { admin3: `${admin}` },
+      { country: `${country}` },
+    ],
+  });
+
+  React.useEffect(() => {
+    const fetcher = async () => {
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 300));
+      const response = await fetch("/data.json");
+      const data = await response.json();
+      setPayload(data.results);
+      setLoading(false);
+    };
+
+    fetcher();
+  }, []);
+
+  const incrementPagination = () => {
+    setPage((currentPage) => currentPage + 10);
+  };
+
   return (
-    <Command label="Command Menu">
-      <Command.Input placeholder="Search for a city, e.g., New York" />
+    <Command shouldFilter={false} loop={false}>
+      <Command.Input
+        placeholder="Miami, Florida, United States"
+        value={searchTerm}
+        onValueChange={setSearchTerm}
+      />
       <Command.List>
-        {/* <Command.Empty>No results found.</Command.Empty> */}
-
-        {/* Ensure unique key and value */}
-        {/* <Command.Group heading="Letters">
-          <Command.Item>a</Command.Item>
-          <Command.Item>b</Command.Item>
-          <Command.Separator />
-          <Command.Item>c</Command.Item>
-        </Command.Group>
-
-        <Command.Item>Apple</Command.Item> */}
+        <Command.Empty>No results found.</Command.Empty>
+        {loading && <Command.Loading>Hang on…</Command.Loading>}
+        {items.slice(0, page).map(({ item: city }) => (
+          <Command.Item
+            key={city.id}
+            onSelect={(value) => console.log("Selected", value)}
+          >
+            {`${city.name}, ${city.admin1}, ${city.country}`}
+          </Command.Item>
+        ))}
       </Command.List>
+      <PaginationButton
+        totalLength={items.length}
+        incrementPagination={incrementPagination}
+      />
     </Command>
+  );
+}
+
+function PaginationButton({ totalLength, incrementPagination }) {
+  const hasPagesLeft = useCommandState(({ filtered }) => {
+    return filtered.count < totalLength;
+  });
+
+  if (!hasPagesLeft) {
+    return null;
+  }
+
+  return (
+    <button type="button" onClick={incrementPagination}>
+      Show more
+    </button>
   );
 }
 
