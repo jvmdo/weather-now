654bd009f728e8008ffc300e7daf83b064b0e3b2
João Oliveira <deoliveira.jvm@gmail.com>
2025-11-06 13:00:13 -0300

    💄 CurrentForecast layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import "./App.css";
-
 // import retry from "/icons/retry.svg";
 // import search from "/icons/search.svg";
 // import error from "/icons/error.svg";
 // import bgLarge from "/images/bg-today-large.svg";
-// import bgSmall from "/images/bg-today-small.svg";
 import "@/lib/day";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import WeatherSearch from "./components/WeatherSearch";
@@ -13,6 +10,7 @@ import Header from "./components/Header";
 
 const queryClient = new QueryClient();
 
+// TODO: Verify text font weights
 function App() {
   return (
     <UnitsContextProvider>
--- src/components/ForecastDashboard/CurrentForecast.jsx
+++ src/components/ForecastDashboard/CurrentForecast.jsx
@@ -1,12 +1,16 @@
+import React from "react";
 import dayjs from "dayjs";
 import { getIconFromWeatherCode } from "@/helpers";
+import { UnitsContext } from "@/contexts";
+import styles from "./CurrentForecast.module.css";
 
 function CurrentForecast({ place, data }) {
+  const { unitShorts } = React.useContext(UnitsContext);
   const icon = getIconFromWeatherCode(data.weather_code);
 
   return (
-    <section className="current-forecast">
-      <div className="hero">
+    <section className={styles.wrapper}>
+      <div className={styles.hero}>
         <header>
           <h2>
             {place[0]}, {place[1]}
@@ -15,14 +19,32 @@ function CurrentForecast({ place, data }) {
             {dayjs(data.time).format("dddd, MMM d, YYYY")}
           </time>
         </header>
-        <img src={`/icons/${icon}.webp`} alt={`weather condition: ${icon}`} />
-        <p>{data.temperature_2m}&deg;</p>
+        <div>
+          <img src={`/icons/${icon}.webp`} alt={`weather condition: ${icon}`} />
+          <strong>{Math.round(data.temperature_2m)}&deg;</strong>
+        </div>
       </div>
-      <div className="variables">
-        <div>Feels like {data.apparent_temperature}&deg;</div>
-        <div>Humidity {data.relative_humidity_2m}%</div>
-        <div>Wind {data.wind_speed_10m}[unit]</div>
-        <div>Precipitation {data.precipitation}[unit]</div>
+      <div className={styles.variables}>
+        <div>
+          <p>Feels like</p>
+          <span>{data.apparent_temperature}&deg;</span>
+        </div>
+        <div>
+          <p>Humidity</p>
+          <span>{data.relative_humidity_2m}%</span>
+        </div>
+        <div>
+          <p>Wind</p>
+          <span>
+            {data.wind_speed_10m} {unitShorts.windSpeed}
+          </span>
+        </div>
+        <div>
+          <p>Precipitation</p>
+          <span>
+            {data.precipitation} {unitShorts.precipitation}
+          </span>
+        </div>
       </div>
     </section>
   );
